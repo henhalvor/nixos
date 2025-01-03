93927ebef5ed52ace4c7600d7f386167d495cdd6
henhalvor <henhalvor@gmail.com>
2025-01-03 16:51:29 +0100

    intial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./nixos/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.henhal = import ./home/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
---
+++ home/home.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "henhal";
+  home.homeDirectory = "/home/henhal";
+  
+  # Ensure home-manager uses same pkgs instance
+  home.stateVersion = "24.11";
+
+  # Let Home Manager install and manage itself
+  programs.home-manager.enable = true;
+
+  # Basic packages you might want
+  home.packages = with pkgs; [
+    git
+    ripgrep
+    fd
+    tree
+    htop
+  ];
+
+  # Git configuration
+  programs.git = {
+    enable = true;
+    userName = "henhalvor";
+    userEmail = "henhalvor@gmail.com"; # Replace with your email
+    extraConfig = {
+      init = {
+	defaultBranch = "main";
+	};
+    };
+  };
+
+  # Shell configuration
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      ll = "ls -l";
+      update = "sudo nixos-rebuild switch";
+    };
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+ # SSH configuration
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      # This configures GitHub specifically
+      "github.com" = {
+        identityFile = "~/.ssh/id_ed25519";
+      };
+    };
+  };
+
+ # Install neovim
+  programs.neovim.enable = true;
+
+  # Manage existing dotfiles
+#  home.file.".config/nvim" = {
+#    source = ./config/nvim;  # This path should point to your nvim config directory
+#    recursive = true;        # Include all subdirectories
+#  };
+}
