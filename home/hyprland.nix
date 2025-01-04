436579f4560224de8a1fa6d4196c123a66b1ab41
henhal <henhalvor@gmail.com>
2025-01-04 20:02:15 +0100

    hyprland possible fix

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -83,34 +83,4 @@
     };
   };
 
-  # Fixing problems with themes: https://wiki.hyprland.org/Nix/Hyprland-on-Home-Manager/
-   home.pointerCursor = {
-      gtk.enable = true;
-      # x11.enable = true;
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 16;
-    };
-
-  gtk = {
-    enable = true;
-
-    theme = {
-      package = pkgs.flat-remix-gtk;
-      name = "Flat-Remix-GTK-Grey-Darkest";
-    };
-
-    iconTheme = {
-      package = pkgs.gnome.adwaita-icon-theme;
-      name = "Adwaita";
-    };
-
-    font = {
-      name = "Sans";
-      size = 11;
-    };
-  };
-
-  # Fix for programs that don’t work in systemd services, but do on the terminal 
-  wayland.windowManager.hyprland.systemd.variables = ["--all"];
 }
