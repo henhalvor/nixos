acc1a393978dc6f9fd3debd07ce79b57babd54cf
henhal <henhalvor@gmail.com>
2025-01-05 13:22:48 +0100

    Hyprland config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -14,9 +14,19 @@
     systemd.enable = true;
     xwayland.enable = true;
     settings = {
-      "$mod" = "SUPER";
+     general = {
+        "$mod" = "SUPER";
+        layout = "dwindle";
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgb(98971A) rgb(CC241D) 45deg";
+        "col.inactive_border" = "0x00000000";
+        border_part_of_window = false;
+        no_border_on_floating = false;
+      };
       bindm = [
-        "$mod, Return, exec, kitty"
+        "$mod, Return, exec, ghostty"
       ];
       input = {
         "kb_layout" = "no";
