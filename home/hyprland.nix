717d29a1d95c8a271644c57446f5d2604b283430
henhal <henhalvor@gmail.com>
2025-01-05 13:29:03 +0100

    .

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -16,14 +16,6 @@
     settings = {
      general = {
         "$mod" = "SUPER";
-        layout = "dwindle";
-        gaps_in = 5;
-        gaps_out = 10;
-        border_size = 2;
-        "col.active_border" = "rgb(98971A) rgb(CC241D) 45deg";
-        "col.inactive_border" = "0x00000000";
-        border_part_of_window = false;
-        no_border_on_floating = false;
       };
       bindm = [
         "$mod, Return, exec, ghostty"
