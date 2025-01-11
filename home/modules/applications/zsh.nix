d863a10c9bd41ae5c316e8f5a6d9c506a361ba3d
henhal <henhalvor@gmail.com>
2025-01-11 18:09:21 +0100

    try to fix zsh config

--- home/modules/applications/zsh.nix
+++ home/modules/applications/zsh.nix
@@ -1,27 +1,31 @@
 { config, pkgs, ... }:
-
 {
-
-  # Shell configuration
   programs.zsh = {
     enable = true;
     autosuggestion.enable = true;
     oh-my-zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "zsh-autosuggestions"
-        "zsh-syntax-highlighting"
-      ];
-      theme = "frisk"; # You can also specify your custom theme or another built-in one.
+      plugins = [ "git" ]; # only include built-in oh-my-zsh plugins here
+      theme = "frisk";
     };
-    syntaxHighlighting.enable = true;
+
+    # Enable syntax highlighting via the nix package
+    syntaxHighlighting = {
+      enable = true;
+      package = pkgs.zsh-syntax-highlighting;
+    };
+
+    # Enable autosuggestions via the nix package
+    enableAutosuggestions = true;
+    autosuggestions.package = pkgs.zsh-autosuggestions;
+
     initExtra = ''
       # Load secrets
       if [ -f "$HOME/.dotfiles/home/secrets/load-secrets.sh" ]; then
         source "$HOME/.dotfiles/home/secrets/load-secrets.sh"
       fi
     '';
+
     history = {
       size = 10000;
       path = "${config.xdg.dataHome}/zsh/history";
@@ -30,12 +34,4 @@
       share = true;
     };
   };
-
-  # Install zsh plugins
-  home.packages = with pkgs; [
-    zsh-syntax-highlighting
-    zsh-autosuggestions
-  ];
-
-
 }
