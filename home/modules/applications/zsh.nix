b14a84635dbcaa06a68c4edc413504c0018600e4
henhal <henhalvor@gmail.com>
2025-01-11 18:53:07 +0100

    p10k config

--- home/modules/applications/zsh.nix
+++ home/modules/applications/zsh.nix
@@ -5,7 +5,6 @@
     oh-my-zsh = {
       enable = true;
       plugins = [ "git" ]; # only include built-in oh-my-zsh plugins here
-      theme = "frisk";
     };
 
     # Enable syntax highlighting
@@ -16,11 +15,23 @@
     # Enable autosuggestions
     autosuggestion.enable = true;
 
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+    ];
+
+
+
     initExtra = ''
       # Load secrets
       if [ -f "$HOME/.dotfiles/home/secrets/load-secrets.sh" ]; then
         source "$HOME/.dotfiles/home/secrets/load-secrets.sh"
       fi
+
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
     '';
 
     history = {
@@ -31,4 +42,8 @@
       share = true;
     };
   };
+
+
+  # Link your p10k config file
+  home.file.".p10k.zsh".source = ../../config/p10k.zsh; # Assuming p10k.zsh is in the same directory as zsh.nix
 }
