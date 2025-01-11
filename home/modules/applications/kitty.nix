fabaa51108acba530c5ce1d9b41f84a48e23af9d
henhal <henhalvor@gmail.com>
2025-01-11 17:52:27 +0100

    Fixed broken kitty config

--- home/modules/applications/kitty.nix
+++ home/modules/applications/kitty.nix
@@ -3,11 +3,5 @@
 {
   programs.kitty = {
     enable = true;
-    font = {
-      family = "Fira Code";
-      size = 12;
-    };
-    scrollback_lines = 10000;
-    window_title_format = "Kitty Terminal";
   };
 }
