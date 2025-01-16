9a15fe3cacbd95614ee02e27695594724356b494
henhal <henhalvor@gmail.com>
2025-01-16 19:07:35 +0100

    Add ssh keygen for github

--- home/modules/settings/git.nix
+++ home/modules/settings/git.nix
@@ -11,15 +11,40 @@
       };
     };
   };
+  #
+  # # SSH configuration
+  # programs.ssh = {
+  #   enable = true;
+  #   matchBlocks = {
+  #     # This configures GitHub specifically
+  #     "github.com" = {
+  #       identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
+  #     };
+  #   };
+  # };
+  #
 
   # SSH configuration
   programs.ssh = {
     enable = true;
     matchBlocks = {
-      # This configures GitHub specifically
       "github.com" = {
-        identityFile = "${config.home.homeDirectory}/.ssh/id_ed25519";
+        identityFile = "${config.home.homeDirectory}/.ssh/github";
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
       };
     };
+  };
+
+  # Generate SSH key if it doesn't exist
+  home.activation = {
+    generateGitHubSSHKey = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ ! -f "${config.home.homeDirectory}/.ssh/github" ]; then
+        ${pkgs.openssh}/bin/ssh-keygen -t ed25519 -C "${userSettings.email}" -f "${config.home.homeDirectory}/.ssh/github" -N ""
+        echo "New SSH key generated for GitHub. Please add this public key to your GitHub account:"
+        cat "${config.home.homeDirectory}/.ssh/github.pub"
+      fi
+    '';
   };
 }
