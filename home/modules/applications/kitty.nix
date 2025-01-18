eab73c1c31034373434954ba39b38fe39f3049ef
henhal <henhalvor@gmail.com>
2025-01-18 12:18:03 +0100

    Kitty config
    -- font
    -- opacity

--- home/modules/applications/kitty.nix
+++ home/modules/applications/kitty.nix
@@ -3,5 +3,21 @@
 {
   programs.kitty = {
     enable = true;
+    settings = {
+      background_opacity = "0.5";  # 0.9 means 90% opaque
+      # Optional: disable opacity in fullscreen
+      dynamic_background_opacity = "yes";
+      # Optional: if you want transparency to work well with background blur
+      background_blur = "1";
+
+      # Font settings
+      font_family = "Hack Nerd Font";
+      font_size = "10";
+      
+      # Optional font tweaks
+      bold_font = "Hack Nerd Font Bold";
+      italic_font = "Hack Nerd Font Italic";
+      bold_italic_font = "Hack Nerd Font Bold Italic";
+    };
   };
 }
