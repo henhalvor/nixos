cacdaed33b73d25fb39cff370c5cf4adf8add1f8
henhal <henhalvor@gmail.com>
2025-02-26 18:52:26 +0100

    Add configuration.nix for desktop + server

---
+++ systems/desktop/configuration.nix
@@ -0,0 +1,14 @@
+
+{ config, pkgs, userSettings, systemSettings, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../nixos/default.nix
+      ../../nixos/modules/graphics.nix
+      ../../nixos/modules/pipewire.nix
+      ../../nixos/modules/bluetooth.nix
+      ../../nixos/modules/bootloader.nix
+      ../../nixos/modules/networking.nix
+    ];
+}
---
+++ systems/hp-server/configuration.nix
@@ -0,0 +1,15 @@
+
+{ config, pkgs, userSettings, systemSettings, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      ../../nixos/default.nix
+      ../../nixos/modules/graphics.nix
+      ../../nixos/modules/ssh.nix
+      ../../nixos/modules/pipewire.nix
+      ../../nixos/modules/bluetooth.nix
+      ../../nixos/modules/bootloader.nix
+      ../../nixos/modules/networking.nix
+    ];
+}
