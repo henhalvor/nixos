a9577941d3c66a0366f29a8457f53c20ed8c94f5
henhal <henhalvor@gmail.com>
2025-03-01 14:23:04 +0100

    Add desktop ssh key

--- nixos/modules/ssh.nix
+++ nixos/modules/ssh.nix
@@ -21,9 +21,11 @@
     # Add your SSH keys here
     # Yoga Pro 7 Ubuntu key (sway)
     # Yoga Pro 7 Nixos key (hyprland)
+    # Desktop ssh key
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjlpTB8/FqToM9BkFuhL7w627YGto8ZYwAdXVR5AT+T henhal@henhal-Yoga-Pro-7-14APH8-Ubuntu"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyX9JZNVAi8TchOBM/bsbYHk6b4adcvqPS+yhCo0r/X henhalvor@gmail.com"
+      "SHA256:2fN3+Fjyq0oix3uxt1zjT1dFLkJY9Yut2bNHyLYv1/E henhalvor@gmail.com"
     ];
   };
 
