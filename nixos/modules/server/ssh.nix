0e4b8fa4529301dd11e85ff035f36c5c4bafb162
henhalvor <133875024+henhalvor@users.noreply.github.com>
2025-04-13 12:22:13 +0200

    Update ssh.nix
    Add key for android phone

--- nixos/modules/server/ssh.nix
+++ nixos/modules/server/ssh.nix
@@ -46,6 +46,7 @@
     # Yoga Pro 7 Nixos key (hyprland)
     # Desktop ssh key TODO: For some reason this does not write to the proper ssh file (has to be added manually)
     openssh.authorizedKeys.keys = [
+"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMt1DD3rn6m5uBKGfgOwAerxKEpOJe54Aood9abQWAvx u0_a514@localhost"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDjlpTB8/FqToM9BkFuhL7w627YGto8ZYwAdXVR5AT+T henhal@henhal-Yoga-Pro-7-14APH8-Ubuntu"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAyX9JZNVAi8TchOBM/bsbYHk6b4adcvqPS+yhCo0r/X henhalvor@gmail.com"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN05rLpXKWvbcdus29yl6d1uC18LKPqe9HLpiXUB9mxp henhalvor@gmail.com"
