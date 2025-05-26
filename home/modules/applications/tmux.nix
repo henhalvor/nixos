99663db4a27b29db52b575ccf039316f19a353c4
henhal <henhalvor@gmail.com>
2025-05-26 19:11:48 +0200

    new tmux config

--- home/modules/applications/tmux.nix
+++ home/modules/applications/tmux.nix
@@ -5,13 +5,13 @@
   ...
 }: let
   # Status bar configuration using Machiato colors
-  statusBar = ''
-    # Status bar styling
-    set -g status "on"
-    set -g status-position top
-    set -g status-justify "left"
-  '';
-
+  # statusBar = ''
+  #   # Status bar styling
+  #   set -g status "on"
+  #   set -g status-position top
+  #   set -g status-justify "left"
+  # '';
+  #
   is_vim =
     pkgs.writeShellScriptBin "is_vim.sh"
     /*
@@ -79,8 +79,81 @@ in {
        # Theme
        set -g @catppuccin_flavor 'macchiato' # latte, frappe, macchiato or mocha
 
+       #
+       # Old commented out config
+       #
+
+       # Status bar styling
+
+       # set -g status "on"
+       # set -g status-position top
+       #
+       # set -g @catppuccin_window_left_separator ""
+       # set -g @catppuccin_window_right_separator " "
+       # set -g @catppuccin_window_middle_separator " █"
+       # set -g @catppuccin_window_number_position "right"
+       #
+       # set -g @catppuccin_window_default_fill "number"
+       # set -g @catppuccin_window_default_text "#W"
+       #
+       # set -g @catppuccin_window_current_fill "number"
+       # set -g @catppuccin_window_current_text "#W"
+       #
+       # set -g @catppuccin_status_modules_right "directory session"
+       # set -g @catppuccin_status_left_separator  " "
+       # set -g @catppuccin_status_right_separator ""
+       # set -g @catppuccin_status_right_separator_inverse "no"
+       # set -g @catppuccin_status_fill "icon"
+       # set -g @catppuccin_status_connect_separator "no"
+       #
+       # set -g @catppuccin_directory_text "#{pane_current_path}"
+
+       #
+       #
+       #
+
+
+      #
+      # New config
+      #
+
+      # Options
+      set -sg terminal-overrides ",*:RGB"  # true color support
+      set -g escape-time 0  # disable delays on escape sequences
+      set -g mouse on
+      set -g renumber-windows on  # keep numbering sequential
+      set -g repeat-time 1000  # increase "prefix-free" window
+
+      # Options: start indexes from 1
+      set -g base-index 1
+      set -g pane-base-index 1
+
+           # Theme: borders
+      set -g pane-border-lines simple
+      set -g pane-border-style fg=black,bright
+       set -g pane-active-border-style fg=#89b4fa
+
+      # Theme: status
+      set -g status-style bg=default,fg=black,bright
+      set -g status-left ""
+      set -g status-right "#[fg=black,bright]#S"
+
+      # Theme: status (windows)
+       set -g window-status-format "#[fg=gray]●"
+      set -g window-status-current-format "●"
+       set -g window-status-current-style "#{?window_zoomed_flag,fg=yellow,fg=#89b4fa,nobold}"
+      set -g window-status-bell-style "fg=red,nobold"
+
+      #
+      #
+      #
+
+       # Reload config
+       bind r source-file ~/.config/tmux/tmux.conf
+
+
        # Vim movement
-      bind-key -n 'C-h' if-shell '${is_vim}/bin/is_vim.sh' 'send-keys C-h' 'select-pane -L'
+       bind-key -n 'C-h' if-shell '${is_vim}/bin/is_vim.sh' 'send-keys C-h' 'select-pane -L'
        bind-key -n 'C-j' if-shell '${is_vim}/bin/is_vim.sh' 'send-keys C-j' 'select-pane -D'
        bind-key -n 'C-k' if-shell '${is_vim}/bin/is_vim.sh' 'send-keys C-k' 'select-pane -U'
        bind-key -n 'C-l' if-shell '${is_vim}/bin/is_vim.sh' 'send-keys C-l' 'select-pane -R'
@@ -88,37 +161,14 @@ in {
        bind-key -T copy-mode-vi 'C-h' select-pane -L
        bind-key -T copy-mode-vi 'C-j' select-pane -D
        bind-key -T copy-mode-vi 'C-k' select-pane -U
-       bind-key -T copy-mode-vi 'C-l' select-pane -R      # Status bar styling
-       set -g status "on"
-       set -g status-position top
+       bind-key -T copy-mode-vi 'C-l' select-pane -R
 
-       set -g @catppuccin_window_left_separator ""
-       set -g @catppuccin_window_right_separator " "
-       set -g @catppuccin_window_middle_separator " █"
-       set -g @catppuccin_window_number_position "right"
 
-       set -g @catppuccin_window_default_fill "number"
-       set -g @catppuccin_window_default_text "#W"
-
-       set -g @catppuccin_window_current_fill "number"
-       set -g @catppuccin_window_current_text "#W"
-
-       set -g @catppuccin_status_modules_right "directory session"
-       set -g @catppuccin_status_left_separator  " "
-       set -g @catppuccin_status_right_separator ""
-       set -g @catppuccin_status_right_separator_inverse "no"
-       set -g @catppuccin_status_fill "icon"
-       set -g @catppuccin_status_connect_separator "no"
-
-       set -g @catppuccin_directory_text "#{pane_current_path}"
-
-       # Reload config
-       bind r source-file ~/.config/tmux/tmux.conf
 
 
        # Additional quality of life settings
        set -g focus-events on
-       set -sa terminal-features ',xterm-256color:RGB'
+       # set -sa terminal-features ',xterm-256color:RGB'
        set -g automatic-rename on
        set -g renumber-windows on
 
