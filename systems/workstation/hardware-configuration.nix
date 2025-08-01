b3a0587764a6891845264eef86f9401b002e1c26
henhal <henhalvor@gmail.com>
2025-08-01 21:13:43 +0200

    Stable workstation config with NVIDIA GPU

--- systems/workstation/hardware-configuration.nix
+++ systems/workstation/hardware-configuration.nix
@@ -14,27 +14,25 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/ef796441-7631-4092-aa85-500209856bbd";
+    { device = "/dev/disk/by-uuid/c7b1090a-8a60-4825-95cb-d425bd2784f9";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DCA7-53B0";
+    { device = "/dev/disk/by-uuid/B8C8-2FB5";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/37b92ab2-d096-47c9-a5ed-93be857ea6c4"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp5s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- systems/workstation/system-monitoring-commands.md
+++
@@ -1,4 +0,0 @@
-# Quick daily health check
-
-echo "SMU errors today: $(journalctl --since today | grep -i 'SMU uninitialized' | wc -l)"
-echo "Suspends today: $(journalctl --since today | grep -E 'Suspending|suspend entered' | wc -l)"
