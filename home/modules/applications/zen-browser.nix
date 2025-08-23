619185ecd973bd0aa1207dff01d7371ea16c7bba
henhal <henhalvor@gmail.com>
2025-08-23 16:37:42 +0200

    Make zen default browser

--- home/modules/applications/zen-browser.nix
+++ home/modules/applications/zen-browser.nix
@@ -4,20 +4,34 @@
   # Install Zen Browser via home.packages
   home.packages = [ zen-browser.packages.${system}.default ];
 
-  #   xdg.mimeApps = {
-  #     enable = true;
-  #     defaultApplications = {
-  # "application/x-extension-htm" = ["userapp-Zen-CD5312.desktop"];
-  # "application/x-extension-html" = ["userapp-Zen-CD5312.desktop"];
-  # "application/x-extension-shtml" = ["userapp-Zen-CD5312.desktop"];
-  # "application/x-extension-xht" = ["userapp-Zen-CD5312.desktop"];
-  # "application/xhtml+xml" = ["userapp-Zen-CD5312.desktop"];
-  # "text/html" = ["userapp-Zen-CD5312.desktop"];
-  # "x-scheme-handler/http" = ["userapp-Zen-CD5312.desktop"];
-  # "x-scheme-handler/https" = ["userapp-Zen-CD5312.desktop"];
-  # "x-scheme-handler/chrome" = ["userapp-Zen-CD5312.desktop"];
-  # "application/x-extension-xhtml" = ["userapp-Zen-CD5312.desktop"];
-  #    };
-  #   };
-  #
+  xdg.mimeApps = {
+    enable = true;
+
+    defaultApplications = {
+      "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/chrome" = [ "zen-beta.desktop" ];
+      "text/html" = [ "zen-beta.desktop" ];
+      "application/x-extension-htm" = [ "zen-beta.desktop" ];
+      "application/x-extension-html" = [ "zen-beta.desktop" ];
+      "application/x-extension-shtml" = [ "zen-beta.desktop" ];
+      "application/xhtml+xml" = [ "zen-beta.desktop" ];
+      "application/x-extension-xhtml" = [ "zen-beta.desktop" ];
+      "application/x-extension-xht" = [ "zen-beta.desktop" ];
+    };
+
+    associations.added = {
+      "x-scheme-handler/http" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/https" = [ "zen-beta.desktop" ];
+      "x-scheme-handler/chrome" = [ "zen-beta.desktop" ];
+      "text/html" = [ "zen-beta.desktop" ];
+      "application/x-extension-htm" = [ "zen-beta.desktop" ];
+      "application/x-extension-html" = [ "zen-beta.desktop" ];
+      "application/x-extension-shtml" = [ "zen-beta.desktop" ];
+      "application/xhtml+xml" = [ "zen-beta.desktop" ];
+      "application/x-extension-xhtml" = [ "zen-beta.desktop" ];
+      "application/x-extension-xht" = [ "zen-beta.desktop" ];
+    };
+  };
+
 }
