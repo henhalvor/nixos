bd49f8962a2f3c0a7db499f65f330997d3740c86
henhal <henhalvor@gmail.com>
2025-12-13 21:23:27 +0100

    Enable VIAL

--- users/henhal/home.nix
+++ users/henhal/home.nix
@@ -56,7 +56,7 @@
       # ../../home/modules/applications/aider-chat.nix
       ../../home/modules/applications/claude-code.nix # installed via npm
       ../../home/modules/applications/tmux.nix
-      # ../../home/modules/applications/vial.nix
+      ../../home/modules/applications/vial.nix
       ../../home/modules/applications/brave.nix
       # ../../home/modules/applications/vscode.nix
       # ../../home/modules/applications/cursor.nix
