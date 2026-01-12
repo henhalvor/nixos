f8b18ff74dc846aceb3208eae52aa40a4f1f308d
henhal <henhalvor@gmail.com>
2026-01-12 19:57:51 +0100

    Gaming: add minecraft

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,34 +1,47 @@
-{ pkgs, config, ... }: {
-  imports = [ ];
-
-  options = { };
-
+{
+  pkgs,
+  config,
+  ...
+}: {
   # Keeping this as a nixos module for now. The systemPackages could be moved to home-manager if needed. Co-locating gaming stuff here for now.
 
-  config = {
-    programs.gamemode.enable = true; # for performance mode
+  programs.gamemode.enable = true; # for performance mode
 
-    programs.steam = {
-      enable = true; # install steam
-      remotePlay.openFirewall =
-        true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall =
-        true; # Open ports in the firewall for Source Dedicated Server
-    };
-
-    environment.systemPackages = with pkgs; [
-      heroic # install heroic launcher
-      lutris # install lutris launcher
-      mumble # install voice-chat
-      protonup-qt # GUI for installing custom Proton versions like GE_Proton
-      # (retroarch.override {
-      #   cores = with libretro; [ # decide what emulators you want to include
-      #     puae # Amiga 500
-      #     scummvm
-      #     dosbox
-      #   ];
-      # })
-      teamspeak_client # install voice-chat
-    ];
+  programs.steam = {
+    enable = true; # install steam
+    remotePlay.openFirewall =
+      true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall =
+      true; # Open ports in the firewall for Source Dedicated Server
   };
+
+  environment.systemPackages = with pkgs; [
+    heroic # install heroic launcher
+    lutris # install lutris launcher
+    mumble # install voice-chat
+    protonup-qt # GUI for installing custom Proton versions like GE_Proton
+    # (retroarch.override {
+    #   cores = with libretro; [ # decide what emulators you want to include
+    #     puae # Amiga 500
+    #     scummvm
+    #     dosbox
+    #   ];
+    # })
+    teamspeak_client # install voice-chat
+
+    prismlauncher # Minecraft launcher
+    # You can override prismlauncher to change the environment available to the launcher and the game. This might be useful for installing additional versions of Java or providing extra binaries needed by some mods.
+    (prismlauncher.override {
+      # Add binary required by some mod
+      additionalPrograms = [ffmpeg];
+
+      # Change Java runtimes available to Prism Launcher
+      jdks = [
+        graalvm-ce
+        zulu8
+        zulu17
+        zulu
+      ];
+    })
+  ];
 }
