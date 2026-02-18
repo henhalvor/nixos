8749f12754b458c8dad3dfc62cd6de3c31b8186e
henhal <henhalvor@gmail.com>
2026-02-18 20:59:47 +0100

    feat(NVF): add tabout-nvim plugin
    -- allows to press tab to go to end of brackets

--- home/modules/applications/nvf.nix
+++ home/modules/applications/nvf.nix
@@ -2336,6 +2336,32 @@ in {
             '';
           };
 
+          tabout-nvim = {
+            package = pkgs.vimPlugins.tabout-nvim;
+            setup = ''
+              require("tabout").setup({
+              	tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+              	backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+              	act_as_tab = true, -- shift content if tab out is not possible
+              	act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+              	default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+              	default_shift_tab = "<C-d>", -- reverse shift default action,
+              	enable_backwards = true, -- well ...
+              	completion = false, -- if the tabkey is used in a completion pum
+              	tabouts = {
+              		{ open = "'", close = "'" },
+              		{ open = '"', close = '"' },
+              		{ open = "`", close = "`" },
+              		{ open = "(", close = ")" },
+              		{ open = "[", close = "]" },
+              		{ open = "{", close = "}" },
+              	},
+              	ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+              	exclude = {}, -- tabout will ignore these filetypes
+              })
+            '';
+          };
+
           harpoon2 = {
             package = pkgs.vimPlugins.harpoon2;
             setup = ''
@@ -2453,6 +2479,7 @@ in {
           vimPlugins.nvim-ts-context-commentstring # used for mini.comment jsx comment strings
           vimPlugins.rustaceanvim
           unstable.vimPlugins.vscode-diff-nvim
+          vimPlugins.tabout-nvim
 
           # Use this if "built in" persistence is not working
           # vimPlugins.persistence-nvim
