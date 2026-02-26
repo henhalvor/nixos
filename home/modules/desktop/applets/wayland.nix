ef8e8d6e1c639726e497d6621dc8baab25025f3c
henhal <henhalvor@gmail.com>
2026-02-26 16:24:27 +0100

    fix(bluetoth notifications): remove blueman notification when connecting / disconnection headphones

--- home/modules/desktop/applets/wayland.nix
+++ home/modules/desktop/applets/wayland.nix
@@ -1,11 +1,15 @@
-{ config, lib, pkgs, ... }:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   # Wayland system tray applets
-  services.blueman-applet.enable = true;
+  services.blueman-applet.enable = false;
   services.network-manager-applet.enable = true;
 
   home.packages = with pkgs; [
-    blueman
+    # blueman
     networkmanagerapplet
   ];
 }
