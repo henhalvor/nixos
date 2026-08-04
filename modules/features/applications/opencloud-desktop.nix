712567ed9e53a7dd6982d57a6c034137e415aa66
Henrik <henhalvor@gmail.com>
2026-08-04 22:10:18 +0200

    opencloud desktop client fix
    -- fixed missing kvantum package

--- modules/features/applications/opencloud-desktop.nix
+++ modules/features/applications/opencloud-desktop.nix
@@ -11,8 +11,25 @@
 
   flake.homeModules.opencloudDesktop =
     { lib, pkgs, ... }:
+    let
+      # Stylix selects Kvantum for Qt applications.  OpenCloud Desktop is a
+      # Qt 6 application, but its upstream package does not include that style
+      # plugin in its own Qt plugin path.  Wrap only this client so it follows
+      # the existing desktop theme instead of failing at startup.
+      opencloudDesktop = pkgs.symlinkJoin {
+        name = "opencloud-desktop-kvantum";
+        paths = [ pkgs.opencloud-desktop ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/opencloud \
+            --prefix QT_PLUGIN_PATH : ${pkgs.qt6Packages.qtstyleplugin-kvantum}/lib/qt-6/plugins
+          wrapProgram $out/bin/opencloudcmd \
+            --prefix QT_PLUGIN_PATH : ${pkgs.qt6Packages.qtstyleplugin-kvantum}/lib/qt-6/plugins
+        '';
+      };
+    in
     {
-      home.packages = [ pkgs.opencloud-desktop ];
+      home.packages = [ opencloudDesktop ];
 
       home.activation.createOpencloudSyncRoot = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         $DRY_RUN_CMD mkdir -p "$HOME/Cloud"
